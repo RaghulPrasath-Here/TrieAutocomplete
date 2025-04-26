001c0dbde813adc4cc95354d5844d40cb25068a3
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-04-27 03:06:10 +0530

    feat: TrieNode class executed with its base methods

---
+++ TrieNode.java
@@ -0,0 +1,29 @@
+package TrieAutocomplete;
+public class TrieNode {
+    TrieNode[] links = new TrieNode[26];
+    boolean flag = false;
+
+    public TrieNode() {
+    }
+
+    boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void put(TrieNode node, char ch) {
+        links[ch - 'a'] = node;
+    }
+
+    boolean isEnd() {
+        return flag;
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+}

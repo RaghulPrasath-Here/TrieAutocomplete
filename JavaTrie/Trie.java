f299f27eecc00f209f20c0c2d26aa694706c2bbf
RaghulPrasath-Here <raghulprasathhere@gmail.com>
2025-04-28 01:26:28 +0530

    feat: trie autocomplete search Java implemented

--- JavaTrie/Trie.java
+++ JavaTrie/Trie.java
@@ -1,4 +1,6 @@
 package TrieAutocomplete.JavaTrie;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Trie {
     private static TrieNode root;
@@ -45,4 +47,53 @@ public class Trie {
         }
         return node;
     }
+    public static void dfs(TrieNode node, String word, ArrayList<String> words){
+        if(node.isEnd()){
+            words.add(word);
+        }
+        for (int i = 0;i<node.links.length;i++) {
+            TrieNode child = node.links[i];
+            if(child != null) {
+                char ch = (char) ('a' + i);
+                dfs(child, word + ch, words);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Trie trie = new Trie();
+        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> output = new ArrayList<>();
+        boolean stopAdding = true;
+        while(stopAdding){
+            System.out.println("Enter word to be added: ");
+            words.add(scanner.nextLine());
+            System.out.println("Stop Entering? Y or N: ");
+            if (scanner.nextLine().toLowerCase().trim().equals("y")){
+                stopAdding = false;
+            }
+        }
+
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        boolean stopSearching = true;
+        while (stopSearching){
+            ArrayList<String> out = new ArrayList<>();
+            System.out.println("Enter the string to be searched: ");
+            String input = scanner.nextLine();
+            TrieNode prefix = trie.startWith(input);
+            if(prefix != null){
+                dfs(prefix, input, out);
+            }
+            System.out.println(out);
+            System.out.println("Stop Searching? Y or N: ");
+            if (scanner.nextLine().toLowerCase().trim().equals("y")){
+                stopSearching = false;
+            }
+        }
+    }
+
 }
+
